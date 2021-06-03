00c8099b17263864be1f027ee7d961f52dbeff9b
kwseeker <xiaohuileee@gmail.com>
2021-06-03 09:43:53 +0800

    temp

---
+++ 字节码编程.mm
@@ -0,0 +1,222 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x5b57;&#x8282;&#x7801;&#x7f16;&#x7a0b;" FOLDED="false" ID="ID_1939608702" CREATED="1622518642408" MODIFIED="1622518651057" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="1 &#x7b80;&#x4ecb;" POSITION="right" ID="ID_1805922126" CREATED="1622518694923" MODIFIED="1622518910789" STYLE="bubble">
+<edge COLOR="#ff0000"/>
+<node TEXT="1.1 &#x662f;&#x4ec0;&#x4e48;" ID="ID_1308055574" CREATED="1622518772865" MODIFIED="1622519999643"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#23601;&#26159;&#36890;&#36807;&#32534;&#20889;.class&#25991;&#20214;&#32780;&#19981;&#26159;.java&#25991;&#20214;&#36827;&#34892;&#32534;&#31243;&#12290;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="1.2 &#x4f7f;&#x7528;&#x573a;&#x666f;" ID="ID_1219013940" CREATED="1622518785902" MODIFIED="1622518828407">
+<node TEXT="&#x975e;&#x4fb5;&#x5165;&#x5f0f;&#x8bbe;&#x8ba1;(&#x5982;&#xff1a;&#x94fe;&#x8def;&#x8ffd;&#x8e2a;&#x7cfb;&#x7edf;)" ID="ID_229280557" CREATED="1622519275563" MODIFIED="1622521462221"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#23601;&#26159;&#21407;&#26412;&#30340;&#19994;&#21153;&#20195;&#30721;&#25105;&#19968;&#28857;&#37117;&#19981;&#24819;&#25913;&#12290;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#38656;&#35201;&#30340;&#21151;&#33021;&#20840;&#37096;&#22312;&#22806;&#36793;&#23454;&#29616;&#65292;&#26368;&#21518;&#25554;&#20837;&#21040;&#21407;&#20195;&#30721;&#30340;&#23383;&#33410;&#30721;&#20013;&#12290;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="AOP-&gt;&#x52a8;&#x6001;&#x4ee3;&#x7406;-&gt;&#x5b57;&#x8282;&#x7801;&#x7f16;&#x7a0b;" ID="ID_981917924" CREATED="1622521530150" MODIFIED="1622521550221"/>
+</node>
+</node>
+<node TEXT="1.3 &#x539f;&#x7406;" ID="ID_128139043" CREATED="1622518814928" MODIFIED="1622518838382"/>
+</node>
+<node TEXT="2 &#x4f7f;&#x7528;" POSITION="right" ID="ID_775859151" CREATED="1622518719318" MODIFIED="1622518911586" STYLE="bubble">
+<edge COLOR="#0000ff"/>
+<node TEXT="2.1 &#x6846;&#x67b6;&#xff06;&#x5de5;&#x5177;" ID="ID_513928261" CREATED="1622518884006" MODIFIED="1622518912273" STYLE="bubble">
+<node TEXT="ASM" ID="ID_277895809" CREATED="1622519120019" MODIFIED="1622519477420" STYLE="bubble"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000c0">&#38656;&#35201;&#23545;JVM&#26377;&#19968;&#23450;&#20102;&#35299;</font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ASM &#26159;&#19968;&#20010; Java &#23383;&#33410;&#30721;&#25805;&#25511;&#26694;&#26550;&#12290;&#23427;&#33021;&#34987;&#29992;&#26469;&#21160;&#24577;&#29983;&#25104;&#31867;&#25110;&#32773;&#22686;&#24378;&#26082;&#26377;&#31867;&#30340;&#21151;&#33021;&#12290;ASM &#21487;&#20197;&#30452;
+    </p>
+    <p>
+      &#25509;&#20135;&#29983;&#20108;&#36827;&#21046; class &#25991;&#20214;,&#20063;&#21487;&#20197;&#22312;&#31867;&#34987;&#21152;&#36733;&#20837; Java &#34394;&#25311;&#26426;&#20043;&#21069;&#21160;&#24577;&#25913;&#21464;&#31867;&#34892;&#20026;&#12290;Java class &#34987;
+    </p>
+    <p>
+      &#23384;&#20648;&#22312;&#20005;&#26684;&#26684;&#24335;&#23450;&#20041;&#30340; .class &#25991;&#20214;&#37324;,&#36825;&#20123;&#31867;&#25991;&#20214;&#25317;&#26377;&#36275;&#22815;&#30340;&#20803;&#25968;&#25454;&#26469;&#35299;&#26512;&#31867;&#20013;&#30340;&#25152;&#26377;&#20803;&#32032;:&#31867;
+    </p>
+    <p>
+      &#21517;&#31216;&#12289;&#26041;&#27861;&#12289;&#23646;&#24615;&#20197;&#21450; Java &#23383;&#33410;&#30721;(&#25351;&#20196;)&#12290;ASM &#20174;&#31867;&#25991;&#20214;&#20013;&#35835;&#20837;&#20449;&#24687;&#21518;,&#33021;&#22815;&#25913;&#21464;&#31867;&#34892;&#20026;,
+    </p>
+    <p>
+      &#20998;&#26512;&#31867;&#20449;&#24687;,&#29978;&#33267;&#33021;&#22815;&#26681;&#25454;&#29992;&#25143;&#35201;&#27714;&#29983;&#25104;&#26032;&#31867;&#12290;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Javaassist" ID="ID_64805459" CREATED="1622519124371" MODIFIED="1622519238336" STYLE="bubble"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000c0">&#22522;&#20110;ASM</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Byte Buddy" ID="ID_1416223891" CREATED="1622519133634" MODIFIED="1622519239037" STYLE="bubble"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000c0">&#22522;&#20110;ASM</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="3 &#x9644;&#x5f55;" POSITION="right" ID="ID_1051133559" CREATED="1622519688245" MODIFIED="1622519698910" STYLE="bubble">
+<edge COLOR="#00ff00"/>
+<node TEXT="&#x300a;ASM&#x4f7f;&#x7528;&#x6307;&#x5357;&#x300b;" ID="ID_1770956740" CREATED="1622519700271" MODIFIED="1622519724504"/>
+<node TEXT="Java&#x5b57;&#x8282;&#x7801;&#x6307;&#x4ee4;&#x8868;" ID="ID_1158900596" CREATED="1622519724857" MODIFIED="1622519745547"/>
+<node TEXT="IDEA&#x63d2;&#x4ef6;&#xff1a;" ID="ID_1306821346" CREATED="1622521577686" MODIFIED="1622522298694">
+<node TEXT="jclasslib bytecode viewer" ID="ID_1906567966" CREATED="1622522282634" MODIFIED="1622522304457"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://plugins.jetbrains.com/plugin/9248-jclasslib-bytecode-viewer">https://plugins.jetbrains.com/plugin/9248-jclasslib-bytecode-viewer</a> 
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="ASM Bytecode Outline" ID="ID_1996204019" CREATED="1622522279916" MODIFIED="1622522721815"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21487;&#33258;&#21160;&#29983;&#25104;&#28304;&#30721;&#23545;&#24212;&#20351;&#29992;ASM&#24320;&#21457;&#30340;&#20195;&#30721;&#65292;&#22522;&#26412;&#19978;&#23601;&#21482;&#38656;&#35201;&#20250;&#25913;&#23601;&#34892;&#20102;&#12290;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#21478;&#22806;&#36824;&#33021;&#29983;&#25104;&#23545;&#24212;&#30340;&#23383;&#33410;&#30721;&#12289;groovy&#20195;&#30721;&#12290;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
